65cfbb4f2bc7c4a675a51c4e783ab139590efe67
Chase Rensberger <chasehrensberger@gmail.com>
2025-11-28 00:31:47 -0500

    checkpoint

--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"balance/ui"
 	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	m, err := ui.NewPrimaryModel()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	p := tea.NewProgram(m, tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
+	fmt.Println("balance")
 }
